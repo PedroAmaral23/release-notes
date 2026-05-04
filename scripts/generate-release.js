17f2bca4498d1f2872e79ee58bdd0b40349addb0
Pedro Amaral <pedrohta007@gmail.com>
2026-05-04 15:29:22 -0300

    bugfix/2/ajustes de release AB#2

--- scripts/generate-release.js
+++ scripts/generate-release.js
@@ -58,7 +58,17 @@ async function main() {
     }
   })
 
-  let output = `🚀 Publicação em: ${new Date().toLocaleString()}\n\n`
+  const now = new Date().toLocaleString("pt-BR", {
+    timeZone: "America/Sao_Paulo",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false
+    })
+
+  let output = `🚀 Publicação em: ${now}\n\n`
 
   if (grouped.feature.length) {
     output += "✨ Funcionalidades\n"
